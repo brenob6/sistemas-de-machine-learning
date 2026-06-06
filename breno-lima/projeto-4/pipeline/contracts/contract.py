044956eb663945fd28edc88e0b55f7c2baf94662
Breno <brenob6@gmail.com>
2026-06-06 14:16:56 -0300

    add: semantic contract

---
+++ breno-lima/projeto-4/pipeline/contracts/contract.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class IndicadoresContract(BaseModel):
+    lucro_liquido: float
+    roe: float
+    lucro_liquido_recorrente: float
+    lucro_liquido_por_acao: float
+    divida_liquida: float
+    patrimonio_liquido: float
+    nav: float
+    valor_mercado: float
+    volume_medio_diario: float
+
+
+class ResultadoIndividualContract(BaseModel):
+    itau: float
+    dexco: float
+    alpargatas: float
+    motiva: float
+    aegea: float
+    copa_engenharia: float
+
+    despesas_administrativas: float
+    despesas_tributarias: float
+    doacoes: float
+
+
+class ItausaContract(BaseModel):
+    indicadores: IndicadoresContract
+    resultado_individual: ResultadoIndividualContract
