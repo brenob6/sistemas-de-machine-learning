b95cbcf67cb207921584c868cdc628a0e7fb5f9d
Breno <brenob6@gmail.com>
2026-06-07 15:10:21 -0300

    fix: handle model erro

--- breno-lima/projeto-4/pipeline/model/gemini.py
+++ breno-lima/projeto-4/pipeline/model/gemini.py
@@ -19,17 +19,21 @@ class GeminiModel:
     def prompt(self, contents: str) -> str | None:
         client = genai.Client(api_key=api_key)
 
-        response = client.models.generate_content(
-            model="gemini-3-flash-preview",
-            config=genai.types.GenerateContentConfig(
-                system_instruction=f"Your job are to analyze markdown tables and extract the information in a structured format. You should return the information in a JSON format, with the following structure:{self.semantic_structure.model_json_schema()}",
-                response_schema=ItausaContract,
-                response_mime_type="application/json",
-            ),
-            contents=contents,
-        )
+        try:
+            response = client.models.generate_content(
+                model="gemini-3-flash-preview",
+                config=genai.types.GenerateContentConfig(
+                    system_instruction=f"Your job are to analyze markdown tables and extract the information in a structured format. You should return the information in a JSON format, with the following structure:{self.semantic_structure.model_json_schema()}",
+                    response_schema=ItausaContract,
+                    response_mime_type="application/json",
+                ),
+                contents=contents,
+            )
 
-        return response.text
+            return response.text
+        except Exception as e:
+            print(f"Error generating content: {e}")
+            return None
 
 
 geminiModel = GeminiModel()
